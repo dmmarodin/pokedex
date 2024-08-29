a02e89edf8eca3b98fc27352b21bc3fb359c9c8d
Douglas Marodin <douglasmarodin@gmail.com>
2024-08-29 17:28:44 -0300

    [add] basic styling and scaffolding for angular app

--- app/src/app/pokedex-app.module.ts
+++ app/src/app/pokedex-app.module.ts
@@ -4,19 +4,25 @@ import { RouterModule } from "@angular/router";
 
 import { routes } from './pokedex-app.routes';
 import { PokedexAppComponent } from "./pokedex-app.component";
-import { PokemonService } from "./pokemon/pokemon.service";
+import { PokemonService } from "./services/pokemon.service";
+import { CardComponent } from "./shared/card/card.component";
+import { LogoComponent } from "./shared/logo/logo.component";
+import { provideHttpClient, withInterceptorsFromDi } from "@angular/common/http";
 
 @NgModule({
   imports: [
     BrowserModule,
     RouterModule.forRoot(routes),
+    CardComponent,
+    LogoComponent
   ],
   providers: [
     PokemonService,
+    provideHttpClient(withInterceptorsFromDi())
   ],
   declarations: [
     PokedexAppComponent,
   ],
   bootstrap: [PokedexAppComponent],
 })
-export class PokedexAppModule {}
+export class PokedexAppModule { }
